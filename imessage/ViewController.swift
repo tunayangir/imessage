1f513c08c28fdb6fcabdc0a66a7928fe4ed0a935
TunaYANGIR <TunaYANGIR@tunayangir-mbp.mynet>
2016-09-20 13:36:37 +0300

    Initial Commit

---
+++ imessage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  imessage
+//
+//  Created by TunaYANGIR on 20.09.2016.
+//  Copyright © 2016 TunaYANGIR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
